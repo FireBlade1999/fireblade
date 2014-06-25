44f24007f015e335d78d1d963a7378e6fc90f3a7
Steve Williams <steven.williams@vibrantmedia.com>
2014-06-25 11:33:17 +0100

    Added handle for requests.
    Change-Id: Ic042774398e53b312d1e479454db1a8d9fa19e18

--- src/main/java/org/fireblade/pixel/server/FireBladeController.java
+++ src/main/java/org/fireblade/pixel/server/FireBladeController.java
@@ -1,12 +1,21 @@
 package org.fireblade.pixel.server;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
- *
+ * Handle requests
+ * 
  * @author swilliams
  */
 @Controller
 public class FireBladeController {
 
+    @RequestMapping(value = {"/fb"}, method = RequestMethod.GET)
+    public void handle(HttpServletRequest request, HttpServletResponse response) {
+        System.out.println(request.getHeaderNames());
+    }
 }
