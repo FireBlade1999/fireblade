ca346dff20ecc6f9ce0a95345f8a97ab4c7c2d1b
Steve Williams <steven.williams@vibrantmedia.com>
2014-06-25 14:32:15 +0100

    Added logPageView.
    Change-Id: Iedc914e661a7d1d361fa44477a491fb96f14bc44

--- src/main/java/org/fireblade/pixel/server/FireBladeController.java
+++ src/main/java/org/fireblade/pixel/server/FireBladeController.java
@@ -1,5 +1,10 @@
 package org.fireblade.pixel.server;
 
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
@@ -7,15 +12,48 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
- * Handle requests
+ * Handle all requests to "/fb" and log a page view if able.
  * 
  * @author swilliams
  */
 @Controller
 public class FireBladeController {
 
+    /**
+     * Get the User object from the request and log a page view.
+     * 
+     * @param request the HttpServletRequest to read from
+     * @param response the HttpServletReponse to write to
+     */
     @RequestMapping(value = {"/fb"}, method = RequestMethod.GET)
     public void handle(HttpServletRequest request, HttpServletResponse response) {
-        System.out.println(request.getHeaderNames());
+        User user = (User) request.getAttribute("user");
+        
+        logPageView(request, user);
+    }
+    
+    /**
+     * Get the header information from the request and log the page view.
+     * 
+     * @param request the HttpServletRequest to read from
+     * @param user the User object associated with this request
+     */
+    private void logPageView(HttpServletRequest request, User user) {
+        String userAgent = request.getHeader("User-Agent");
+        String language = request.getLocale().getLanguage();
+        String rawUrl = request.getHeader("Referer");
+        
+        Logger logger = Logger.getLogger("fireblade-server.log");
+        FileHandler fileHandler;
+        
+        try {
+            fileHandler = new FileHandler("/src/main/resources/fireblade-server.log");
+            logger.addHandler(fileHandler);
+            SimpleFormatter formatter = new SimpleFormatter();
+            fileHandler.setFormatter(formatter);
+            logger.log(Level.INFO, "User: {0} rawUrl: {1}", new Object[]{user.getId(), rawUrl});
+        } catch (IOException | SecurityException e) {
+            // TODO - handle
+        }
     }
 }
