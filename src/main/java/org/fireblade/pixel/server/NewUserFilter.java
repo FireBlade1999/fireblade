8f55cd937e745d2722c44da7cd0cc738ca3f466d
Steve Williams <steven.williams@vibrantmedia.com>
2014-06-25 14:27:12 +0100

    Added NewUserFilter to check requests for cookie and take appropriate action.
    Change-Id: Idece9676bfa52ba0b75354c1692c53466d217367

---
+++ src/main/java/org/fireblade/pixel/server/NewUserFilter.java
@@ -0,0 +1,39 @@
+package org.fireblade.pixel.server;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Add a user cookie to the response if none is present in the request.
+ * 
+ * @author swilliams
+ */
+public class NewUserFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        // do nothing
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+        
+        // proceed with request
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+        // do nothing
+    }
+
+}
